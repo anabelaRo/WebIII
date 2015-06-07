fb149c7f6d4383c3f96d1ddae64bde18ed6e9746
Anabela <anabela.rossi@gmail.com>
2015-06-07 13:09:16 -0300

    Agrego entities BBDD
    Dentro de la carpeta BBDD están los datos de prueba

---
+++ BBDD/inserts_prueba.sql
@@ -0,0 +1,29 @@
+------------------------ TORNEOS
+INSERT INTO dbo.Torneo VALUES (1, 'Torneo 1', 1);
+INSERT INTO dbo.Torneo VALUES (2, 'Torneo 2', 1);
+INSERT INTO dbo.Torneo VALUES (3, 'Torneo 3', 0);
+INSERT INTO dbo.Torneo VALUES (4, 'Torneo 4', 1);
+INSERT INTO dbo.Torneo VALUES (5, 'Torneo 5', 0);
+------------------------ EQUIPOS
+INSERT INTO Dbo.Equipo VALUES (1, 'Racing', 100, 1);
+INSERT INTO Dbo.Equipo VALUES (2, 'River', 100, 1);
+INSERT INTO Dbo.Equipo VALUES (3, 'Boca', 100, 2);
+INSERT INTO Dbo.Equipo VALUES (4, 'Velez', 100, 1);
+INSERT INTO Dbo.Equipo VALUES (5, 'San Lorenzo', 100, 3);
+INSERT INTO Dbo.Equipo VALUES (6, 'Rosario Central', 100, 1);
+INSERT INTO Dbo.Equipo VALUES (7, 'Barcelona', 100, 4);
+INSERT INTO Dbo.Equipo VALUES (8, 'Real Madrid', 100, 4);
+INSERT INTO Dbo.Equipo VALUES (9, 'Villareal', 100, 4);
+INSERT INTO Dbo.Equipo VALUES (10, 'Valencia', 100, 4);
+------------------------ JUGADORES
+insert into dbo.Jugador values (1, 'Sebastian',  'Saja', 35, 1);
+insert into dbo.Jugador values (2, 'Gabriel',  'Mercado', 28, 2);
+insert into dbo.Jugador values (3, 'Fernando',  'Gago', 29, 3);
+insert into dbo.Jugador values (4, 'Sebastian',  'Dominguez', 28, 4);
+insert into dbo.Jugador values (5, 'Nestor',  'Ortigoza', 30, 5);
+insert into dbo.Jugador values (6, 'Marco',  'Ruben', 28, 6);
+insert into dbo.Jugador values (7, 'Karim',  'Benzema', 27, 8);
+insert into dbo.Jugador values (8, 'Luciano',  'Vietto', 21, 9);
+insert into dbo.Jugador values (9, 'Rodrigo',  'De Paul', 21, 10);
+insert into dbo.Jugador values (10, 'Lionel',  'Messi', 28, 7);
+
