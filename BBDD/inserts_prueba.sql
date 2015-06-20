7fdf316d69ecea46fc884ec7598f070541dae9df
Anabela <anabela.rossi@gmail.com>
2015-06-20 01:18:09 -0300

    Agrego Identity en el script

--- BBDD/inserts_prueba.sql
+++ BBDD/inserts_prueba.sql
@@ -1,29 +1,28 @@
 ------------------------ TORNEOS
-INSERT INTO dbo.Torneo VALUES (1, 'Torneo 1', 1);
-INSERT INTO dbo.Torneo VALUES (2, 'Torneo 2', 1);
-INSERT INTO dbo.Torneo VALUES (3, 'Torneo 3', 0);
-INSERT INTO dbo.Torneo VALUES (4, 'Torneo 4', 1);
-INSERT INTO dbo.Torneo VALUES (5, 'Torneo 5', 0);
+INSERT INTO dbo.Torneo VALUES ('Torneo 1', 1);
+INSERT INTO dbo.Torneo VALUES ( 'Torneo 2', 1);
+INSERT INTO dbo.Torneo VALUES ( 'Torneo 3', 0);
+INSERT INTO dbo.Torneo VALUES ( 'Torneo 4', 1);
+INSERT INTO dbo.Torneo VALUES ( 'Torneo 5', 0);
 ------------------------ EQUIPOS
-INSERT INTO Dbo.Equipo VALUES (1, 'Racing', 100, 1);
-INSERT INTO Dbo.Equipo VALUES (2, 'River', 100, 1);
-INSERT INTO Dbo.Equipo VALUES (3, 'Boca', 100, 2);
-INSERT INTO Dbo.Equipo VALUES (4, 'Velez', 100, 1);
-INSERT INTO Dbo.Equipo VALUES (5, 'San Lorenzo', 100, 3);
-INSERT INTO Dbo.Equipo VALUES (6, 'Rosario Central', 100, 1);
-INSERT INTO Dbo.Equipo VALUES (7, 'Barcelona', 100, 4);
-INSERT INTO Dbo.Equipo VALUES (8, 'Real Madrid', 100, 4);
-INSERT INTO Dbo.Equipo VALUES (9, 'Villareal', 100, 4);
-INSERT INTO Dbo.Equipo VALUES (10, 'Valencia', 100, 4);
+INSERT INTO Dbo.Equipo VALUES ( 'Racing', 100, 1);
+INSERT INTO Dbo.Equipo VALUES ( 'River', 100, 1);
+INSERT INTO Dbo.Equipo VALUES ( 'Boca', 100, 2);
+INSERT INTO Dbo.Equipo VALUES ( 'Velez', 100, 1);
+INSERT INTO Dbo.Equipo VALUES ( 'San Lorenzo', 100, 3);
+INSERT INTO Dbo.Equipo VALUES ( 'Rosario Central', 100, 1);
+INSERT INTO Dbo.Equipo VALUES ( 'Barcelona', 100, 4);
+INSERT INTO Dbo.Equipo VALUES ( 'Real Madrid', 100, 4);
+INSERT INTO Dbo.Equipo VALUES ( 'Villareal', 100, 4);
+INSERT INTO Dbo.Equipo VALUES ( 'Valencia', 100, 4);
 ------------------------ JUGADORES
-insert into dbo.Jugador values (1, 'Sebastian',  'Saja', 35, 1);
-insert into dbo.Jugador values (2, 'Gabriel',  'Mercado', 28, 2);
-insert into dbo.Jugador values (3, 'Fernando',  'Gago', 29, 3);
-insert into dbo.Jugador values (4, 'Sebastian',  'Dominguez', 28, 4);
-insert into dbo.Jugador values (5, 'Nestor',  'Ortigoza', 30, 5);
-insert into dbo.Jugador values (6, 'Marco',  'Ruben', 28, 6);
-insert into dbo.Jugador values (7, 'Karim',  'Benzema', 27, 8);
-insert into dbo.Jugador values (8, 'Luciano',  'Vietto', 21, 9);
-insert into dbo.Jugador values (9, 'Rodrigo',  'De Paul', 21, 10);
-insert into dbo.Jugador values (10, 'Lionel',  'Messi', 28, 7);
-
+insert into dbo.Jugador values ( 'Sebastian',  'Saja', 35, 1);
+insert into dbo.Jugador values ( 'Gabriel',  'Mercado', 28, 2);
+insert into dbo.Jugador values ( 'Fernando',  'Gago', 29, 3);
+insert into dbo.Jugador values ( 'Sebastian',  'Dominguez', 28, 4);
+insert into dbo.Jugador values ( 'Nestor',  'Ortigoza', 30, 5);
+insert into dbo.Jugador values ( 'Marco',  'Ruben', 28, 6);
+insert into dbo.Jugador values ( 'Karim',  'Benzema', 27, 8);
+insert into dbo.Jugador values ( 'Luciano',  'Vietto', 21, 9);
+insert into dbo.Jugador values ( 'Rodrigo',  'De Paul', 21, 10);
+insert into dbo.Jugador values ( 'Lionel',  'Messi', 28, 7);
